419353ec851ab51ee2b4f6db86e74eda72b94c70
heesung6701 <heesung6701@gmail.com>
2023-06-14 22:35:35 +0900

    copyright header 2번 되어 있던 부분 수정

--- waffle/preview/src/main/kotlin/me/donedone/waffle/preview/textinput/WaffleCountViewActivity.kt
+++ waffle/preview/src/main/kotlin/me/donedone/waffle/preview/textinput/WaffleCountViewActivity.kt
@@ -5,13 +5,6 @@
  * Please see full license: https://github.com/TodayDoneDone/donedone-android/blob/main/LICENSE
  */
 
-/*
- * Designed and developed by DoneDone Team 2023.
- *
- * Licensed under the MIT.
- * Please see full license: https://github.com/TodayDoneDone/donedone-android/blob/main/LICENSE
- */
-
 package me.donedone.waffle.preview.textinput
 
 import android.os.Bundle
