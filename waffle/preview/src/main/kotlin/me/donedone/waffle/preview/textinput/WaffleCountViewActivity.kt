54812b05a19f74af1d37d45826d12c4581973ca7
Heesung Youn <heesung6701@gmail.com>
2023-06-16 21:46:46 +0900

    Deep/27 (#28)
    * WaffleCircularProgressDrawable 구현
    
    * waffle 모듈 내 constraintlayout 의존 추가
    
    * WaffleCountView 추가
    
    * WaffleCountView preview 추가
    
    * 테스트 코드명 잘못 되어 있어서 변경
    
    * WaffleButtonTextActivity -> EmptyTestActivity 변경
    
    * WaffleCountView 테스트 코드 추가
    
    * ValueAnimator start 메인 스레드 에서 실행 되도록 수정
    
    * Spannable이 불필요 한 경우는 String 객체 사용 하도록 수정
    
    * Lint 수행
    
    * copyright 중복 제거
    
    * xml 내 닫기 태그 에서 new line 제거
    
    * 디폴트 밸류에 private 추가
    
    * Main.immediate 추가
    
    * 타입 생략 적용
    
    * name shadowed 되고 있어서 이름 변경
    
    * EmptyTestActivity.kt 삭제
    
    * Assert.assertTrue(true) 삭제
    
    * private 추가
    
    * copyright header 2번 되어 있던 부분 수정

---
+++ waffle/preview/src/main/kotlin/me/donedone/waffle/preview/textinput/WaffleCountViewActivity.kt
@@ -0,0 +1,26 @@
+/*
+ * Designed and developed by DoneDone Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/TodayDoneDone/donedone-android/blob/main/LICENSE
+ */
+
+package me.donedone.waffle.preview.textinput
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import me.donedone.waffle.preview.annotations.WaffleSample
+import me.donedone.waffle.preview.databinding.ActivityWaffleCountViewBinding
+
+@WaffleSample(title = "TextLengthInfo Sample Activity")
+class WaffleCountViewActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val binding = ActivityWaffleCountViewBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    binding.editText.doOnTextChanged { text, _, _, _ ->
+      binding.countView.setValue(text?.length ?: 0)
+    }
+  }
+}
---
+++ waffle/src/main/kotlin/me/donedone/waffle/android/widget/drawable/WaffleCircularProgressDrawable.kt
@@ -0,0 +1,112 @@
+/*
+ * Designed and developed by DoneDone Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/TodayDoneDone/donedone-android/blob/main/LICENSE
+ */
+
+package me.donedone.waffle.android.widget.drawable
+
+import android.animation.ValueAnimator
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.CornerPathEffect
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WaffleCircularProgressDrawable : Drawable() {
+  private companion object {
+    private const val DURATION_ANIMATION = 300L
+    private const val PROGRESS_WIDTH = 5f
+    private const val DEGREE_0 = 0f
+    private const val DEGREE_START = 270f
+    private const val DEGREE_360 = 360f
+  }
+
+  private val primaryPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    style = Paint.Style.STROKE
+    strokeWidth = PROGRESS_WIDTH
+    strokeJoin = Paint.Join.ROUND
+    strokeCap = Paint.Cap.ROUND
+    pathEffect = CornerPathEffect(PROGRESS_WIDTH)
+    isDither = true
+    isAntiAlias = true
+  }
+
+  private val secondPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    style = Paint.Style.STROKE
+    strokeWidth = PROGRESS_WIDTH
+    color = Color.GRAY
+    isAntiAlias = true
+  }
+
+  private var ratio = 0f
+
+  private var animator: ValueAnimator? = null
+  private val drawBounds = RectF()
+  override fun draw(canvas: Canvas) {
+    canvas.drawArc(
+      drawBounds,
+      DEGREE_0,
+      DEGREE_360,
+      false,
+      secondPaint,
+    )
+
+    canvas.drawArc(
+      drawBounds,
+      DEGREE_START,
+      ratio * DEGREE_360,
+      false,
+      primaryPaint,
+    )
+  }
+
+  override fun onBoundsChange(bounds: Rect) {
+    super.onBoundsChange(bounds)
+    val sw = bounds.width().coerceAtMost(bounds.height())
+    val radius = sw / 2 - PROGRESS_WIDTH
+    val cx = bounds.centerX().toFloat()
+    val cy = bounds.centerY().toFloat()
+    drawBounds.set(
+      RectF(
+        cx - radius,
+        cy - radius,
+        cx + radius,
+        cy + radius,
+      ),
+    )
+  }
+
+  override fun setAlpha(alpha: Int) {
+    primaryPaint.alpha = alpha
+  }
+
+  override fun setColorFilter(colorFilter: ColorFilter?) {
+    primaryPaint.setColorFilter(colorFilter)
+  }
+
+  @Deprecated("Deprecated in Java")
+  override fun getOpacity() = primaryPaint.alpha
+
+  fun setRatio(to: Float) {
+    CoroutineScope(Dispatchers.Main.immediate).launch {
+      animator?.end()
+      animator = ValueAnimator.ofFloat(ratio, to.coerceIn(0f, 1f))
+        .setDuration(DURATION_ANIMATION)
+        .apply {
+          addUpdateListener {
+            ratio = it.animatedValue as Float
+            invalidateSelf()
+          }
+          start()
+        }
+    }
+  }
+}
